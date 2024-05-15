c94eaad1c3512e43042b99f7119e362e9fecf781
YuliaKletsova <july_2000@mail.ru>
2024-05-15 04:07:43 +0400

    small changes

--- src/FramerMotion/index.tsx
+++ src/FramerMotion/index.tsx
@@ -1,11 +1,45 @@
-import {Flex} from "antd"
+import {Flex, Typography} from "antd"
 import {CloudSVG} from "./CloudSVG"
+import {motion} from "framer-motion"
 
+const divStyles = {
+    width: '100px',
+    height: '100px',
+    background: 'white',
+    borderRadius: '20%'
+}
 export const FramerMotion = () => {
     return  (
-        <Flex style={{padding: '50px'}} vertical align="flex-start" gap={30}>
-            <Flex vertical style={{width: '500px', height: '500px'}}>
+        <Flex style={{padding: '50px'}} align="center" gap={30}>
+            <Flex vertical style={{width: '300px', height: '300px'}}>
                 <CloudSVG />
+            </Flex>
+
+            <Flex vertical style={{width: '300px', height: '300px'}} gap={30} align="center">
+                <Typography.Title level={4}>Hover the box</Typography.Title>
+                <motion.div 
+                    style={divStyles}
+                    whileHover={{ scale: 1.2 }} 
+                    whileTap={{ scale: 0.8 }} 
+                />
+            </Flex>
+
+
+            <Flex vertical style={{width: '300px', height: '300px'}} gap={30} align="center">
+                <Typography.Title level={4}>Just enjoy</Typography.Title>
+                <motion.div
+                    style={divStyles}
+                    animate={{
+                        scale: [1, 1.2, 1.2, 1.1, 1],
+                        rotate: [0, 0, 270, 270, 0],
+                        borderRadius: ["20%", "20%", "50%", "50%", "20%"],
+                    }}
+                    transition={{
+                        repeat: Infinity,
+                        duration: 2,
+                        repeatDelay: 1
+                    }}
+                />
             </Flex>
         </Flex>
     )
--- src/ReactTransitionGroup/Card.tsx
+++ src/ReactTransitionGroup/Card.tsx
@@ -8,18 +8,18 @@ export const Card = () => {
     const nodeRef = useRef(null);
     const duration = 3000
 
+    const defaultStyle = {
+      transition: `opacity ${duration}ms ease-in-out, height ${duration}ms ease-in-out`,
+      opacity: 0,
+      height: 0
+    }
+
     const transitionStyles = {
       entering: { opacity: 1, height: '200px' },
       entered:  { opacity: 1, height: '200px' },
       exiting:  { opacity: 0 },
       exited:  { opacity: 0 },
     };
-
-    const defaultStyle = {
-      transition: `opacity ${duration}ms ease-in-out, height ${duration}ms ease-in-out`,
-      opacity: 0,
-      height: 0
-    }
 
     useEffect(() => {
       setTimeout(() => setToggleOpen(!toggleOpen), 3000)
@@ -36,7 +36,7 @@ export const Card = () => {
                 //@ts-expect-error TS
                 ...transitionStyles[state]
               }}>
-                <Hello />
+                <Text />
               </div>
             )}
           </Transition>
@@ -44,7 +44,7 @@ export const Card = () => {
       );
 }
 
-const Hello = () => (
+const Text = () => (
     <Typography.Paragraph style={{maxWidth: '500px'}}>
         Lorem ipsum dolor sit amet, consectetur adipiscing elit. Integer ut hendrerit nunc. Pellentesque a metus sem. Donec sit amet euismod augue, vel faucibus mi. Pellentesque eu congue eros. Maecenas erat lectus, finibus ut mollis et, ornare vel felis. Ut rhoncus euismod orci, a rhoncus mi laoreet ut. Ut eleifend urna fringilla, hendrerit purus ac, rhoncus justo. Fusce lobortis nec nisl at tincidunt. Ut nec commodo arcu. Fusce a dui ut tellus elementum dignissim in vitae mi. Nunc aliquam id lacus at tempus. Ut tempor erat vitae neque condimentum dapibus. Suspendisse potenti. Morbi massa nibh, faucibus vel congue quis, fringilla quis tellus. Etiam pharetra luctus tempor. Mauris a placerat quam. Nam pulvinar arcu eget arcu posuere, at semper odio dictum. Sed id dignissim tellus, id malesuada ante. Aliquam erat volutpat. Morbi interdum enim odio, sed aliquam velit scelerisque non. In hac habitasse platea dictumst. Maecenas vel nibh quis dolor viverra blandit sit amet non ligula. Suspendisse libero erat, maximus quis ante ut, tristique rhoncus urna. Curabitur ac interdum tortor, id cursus sem. Mauris consequat eros eget venenatis pulvinar. Vivamus suscipit metus a lectus dictum ultrices. Maecenas fringilla quis enim porttitor convallis. Suspendisse sed pretium tortor, quis venenatis leo. Proin nec velit ornare, laoreet tellus in, vulputate dolor. Morbi sagittis gravida auctor. Maecenas et cursus mauris. Pellentesque convallis tristique est et convallis. Lorem ipsum dolor sit amet, consectetur adipiscing elit. Etiam et ligula dapibus, egestas mi sit amet, iaculis ante. Duis ut est velit. Pellentesque placerat, justo ac varius mattis, dolor est commodo nisi, in mollis risus lectus at urna. Suspendisse ipsum velit, dictum non nunc eu, finibus consequat dui. Pellentesque felis velit, efficitur id dignissim eget, tincidunt quis nisl. Curabitur non fringilla ipsum. Suspendisse potenti. Nullam sit amet blandit nulla. Mauris pharetra diam non vestibulum dapibus. Cras pellentesque sapien urna. Curabitur porta vestibulum massa, id consectetur nulla laoreet sit amet.
     </Typography.Paragraph>
